32d2f53b8480ba597229d6759b723cd0e41ffa68
Prankishor <33565966+Prankishor@users.noreply.github.com>
2023-02-24 00:54:57 +0530

    portfolio v1 complete

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -4,20 +4,25 @@ import ContactUs from "../contactus/ContactUs";
 
 const Footer = () => {
     return (
-        <div className="portfolio__footer section__padding" id="contact">
-            <h1 className="gradient__text">Contact</h1>
-            <div className="portfolio__footer-container">
-                <div className="portfolio__footer-form_container">
-                    <ContactUs />
-                </div>
-                <div className="footer-line">
-                    <p className="gradient__text">"A simple hello can lead to a million things!"</p>
+        <div className="developer">
+            <div className="portfolio__footer section__padding" id="contact">
+                <h1 className="gradient__text">Contact</h1>
+                <div className="portfolio__footer-container">
+                    <div className="portfolio__footer-form_container">
+                        <ContactUs />
+                    </div>
+                    <div className="footer-line">
+                        <p className="gradient__text">"A simple hello can lead to a million things!"</p>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className="deve">
                 <p>Developed by Prankishore Talukdar</p>
+                <p> v1.0</p>
             </div>
         </div>
+
+
     )
 }
 
